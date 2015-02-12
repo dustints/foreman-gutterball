c3c18a0c18677e4c4a9cb1bb2d1f2ec336d18add
Dustin Tsang <dtsang@redhat.com>
2015-02-12 14:34:40 -0500

    create a way to translate candlepin keywords to katello keywords

---
+++ app/lib/actions/foreman_gutterball/content_reports/report_decorator.rb
@@ -0,0 +1,94 @@
+module Actions
+  module ForemanGutterball
+    module ContentReports
+      class ReportDecorator
+        def decorate(report)
+          if report.is_a?(Hash)
+            ::Actions::ForemanGutterball::ContentReports::ReportAssociativeArrayDecorator.new(report)
+          elsif report.is_a?(Array)
+            ::Actions::ForemanGutterball::ContentReports::ReportArrayDecorator.new(report) 
+          elsif report.is_a?(String)
+            report
+          elsif report.nil?
+            ""
+          else
+            raise 'unable to decorate this report'
+          end
+        end
+      end
+
+      class ReportAssociativeArrayDecorator
+        def initialize(report)
+          @report = report
+        end
+
+        def key_translation_mapping
+          result = {}
+          @report.keys.each{|key| result[key] = KatelloToCandlepinTranslator.new.to_katello_speak(key.underscore) }
+          result
+        end
+
+        def keys
+          guide = key_translation_mapping
+          guide.keys.map{|key| guide[key] }
+        end
+        
+        def get(key)
+          ReportDecorator.new.decorate(@report[key] || @report[key_translation_mapping.invert[key]])
+        end
+
+        def has_key?(key)
+          @report.keys.include?(key) || key_translation_mapping.values.include?(key)
+        end
+
+        def method_missing(method_name, *arguments, &block)
+          if has_key?(method_name.to_s)
+            get(method_name.to_s)
+          else
+            super
+          end
+        end
+
+        def respond_to_missing?(method_name, include_private = false)
+          has_key?(method_name.to_s) || super
+        end
+
+        def to_json(hash = {})
+          result = {}
+          @report.keys.each do |key|
+            result[key_translation_mapping[key]] = ReportDecorator.new.decorate(@report[key])
+          end
+          result.to_json
+        end
+      end
+ 
+      class KatelloToCandlepinTranslator
+        CP_TO_KAT_SPEAK = {
+          'consumer' => 'system',
+          'owner' => 'system',
+          'compliance' => 'subscription',
+        }
+
+        def to_katello_speak(input)
+          CP_TO_KAT_SPEAK.keys.inject(input.clone) do |result, candlepin_keyword|
+            result.gsub(candlepin_keyword, CP_TO_KAT_SPEAK[candlepin_keyword])
+          end
+        end
+      end
+      
+      class ReportArrayDecorator
+        include Enumerable
+
+        def initialize(*members)
+          @members = members
+        end
+
+        def each(&block)
+          @members.each do |member|
+            block.call(ReportDecorator.new.decorate(member))
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/unit/report_decorator_test.rb
@@ -0,0 +1,38 @@
+require 'debugger'
+require 'json'
+require 'active_support/inflector'
+
+class ReportDecTest < ActiveSupport::TestCase
+  test 'to_json' do
+    example = <<-EOF
+    {
+      "status" : {
+        "status" : "valid",
+        "date" : "2015-02-09T13:27:40.652+0000"
+      },
+      "consumer" : {
+        "facts" : {
+            "cpu.core(s)_per_socket": "4",
+            "cpu.cpu(s)": "4",
+            "cpu.cpu_socket(s)": "1",
+            "cpu.thread(s)_per_core": "1"
+        },
+        "consumerState" : {
+          "created" : "2015-02-09T13:27:35.578+0000",
+          "deleted" : null
+        },
+        "name" : "test-consumer-BEcQrEdg",
+        "owner" : {
+          "displayName" : "ACME Corporation",
+          "key" : "acme_corporation"
+        },
+        "lastCheckin" : "2015-02-09T13:27:37.809+0000",
+        "uuid" : "0668eca4-2efe-4965-a2ea-610027164c4e"
+      }
+    }
+    EOF
+    resp = JSON.parse(example)
+    decorator  = ::Actions::ForemanGutterball::ContentReports::ReportDecorator.new.decorate(resp)
+    assert_equal decorator.system.owner.display_name, 'ACME Corporation'
+  end
+end
